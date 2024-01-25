297548ca53d6442393157833f247e8d38b62a1d4
yumenome <waiyan2015.wy65@gmail.com>
2024-01-25 15:34:26 +0630

    update

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>Laravel</title>
+        <title>testing</title>
         <script src="https://cdn.tailwindcss.com"></script
 
     </head>
